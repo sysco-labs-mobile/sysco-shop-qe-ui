ea6100270776e58c17887503c97cc9f772de46ba
Cristina Alanis <cristina.alanis@sysco.com>
2021-03-31 13:45:09 -0500

    Added ios page objects.

--- src/main/java/com/qe/pages/search/FilterStockTypePage.java
+++ src/main/java/com/qe/pages/search/FilterStockTypePage.java
@@ -1,6 +1,7 @@
 package com.qe.pages.search;
 
 import com.qe.BaseTest;
+import com.qe.pages.orders.OrdersPage;
 import com.qe.utils.TestUtils;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
@@ -10,26 +11,38 @@ public class FilterStockTypePage extends BaseTest {
     TestUtils utils = new TestUtils();
 
     @AndroidFindBy(accessibility = "Navigate up")
-    @iOSXCUITFindBy(id = "")
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeButton[@name=\"Filters\"]")
     private MobileElement closeButton;
 
     @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.view.ViewGroup/android.widget.TextView")
-    @iOSXCUITFindBy(id = "")
+    @iOSXCUITFindBy(xpath = "//XCUIElementTypeStaticText[@name=\"Stock Type\"]")
     private MobileElement stockTypeHeader;
 
     @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.RadioButton")
-    @iOSXCUITFindBy(id = "")
+    @iOSXCUITFindBy(id = "groupBy-0-0")
     private MobileElement firstStockTypeRemoteSelector;
 
     @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[2]/android.widget.RadioButton")
-    @iOSXCUITFindBy(id = "")
+    @iOSXCUITFindBy(id = "groupBy-0-1")
     private MobileElement firstStockTypeStockSelector;
 
     @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[3]/android.widget.RadioButton")
-    @iOSXCUITFindBy(id = "")
+    @iOSXCUITFindBy(id = "groupBy-0-2")
     private MobileElement firstStockTypeDemandSelector;
 
     @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[4]/android.widget.RadioButton")
-    @iOSXCUITFindBy(id = "")
+    @iOSXCUITFindBy(id = "groupBy-0-3")
     private MobileElement firstStockTypeNonStockSelector;
+
+
+
+    public FilterStockTypePage pressStockSelector() {
+        click(firstStockTypeStockSelector);
+        return new FilterStockTypePage();
+    }
+
+    public FilterStockTypePage pressBack() {
+        click(closeButton);
+        return new FilterStockTypePage();
+    }
 }
